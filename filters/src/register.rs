0389131582a20ed6eef80c61d413ed54c3d9a5fa
Yossi Ovadia <yovadia@redhat.com>
2026-08-27 11:27:56 -0700

    test(register): remove duplicate identity_header_guard from expected filters
    Signed-off-by: Yossi Ovadia <yovadia@redhat.com>

--- filters/src/register.rs
+++ filters/src/register.rs
@@ -404,7 +404,6 @@ mod tests {
             "credential_inject",
             "anthropic_validate",
             "anthropic_web_search",
-            "identity_header_guard",
             "request_id",
             "aws_sigv4_sign",
         ];
